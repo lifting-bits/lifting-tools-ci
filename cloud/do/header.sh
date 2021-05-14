eb78694cddd65b93da0a59fdb8faceb08ea820a4
Artem Dinaburg <artem@trailofbits.com>
2021-05-14 13:06:34 -0700

    Set up a home directory and env var, add some debug support

--- cloud/do/header.sh
+++ cloud/do/header.sh
@@ -22,9 +22,27 @@ export DEBIAN_FRONTEND=noninteractive
 export SLACK_HOOK=__SLACK_HOOK__
 export RUN_NAME="__RUN_NAME__"
 export CI_BRANCH=__CI_BRANCH__
+export HOME=/root
+export DEBUG=__DEBUG__
 
+# set up home dir
+mkdir -p ${HOME}
+cd ${HOME}
+
+# Default CI branch to master if not specified
 if [[ "${CI_BRANCH,,}" = "__ci_branch__" ]]
 then
      CI_BRANCH=master
+fi
+
+# set up core dumps
+if [[ "${DEBUG,,}" = "yes" ]]
+then
+  # undo terminate on exit
+  trap - EXIT
+  # set up core files
+  ulimit -c unlimited
+  mkdir -p ${HOME}/core
+  echo "${HOME}/core/core.%p.%e.%t" > /proc/sys/kernel/core_pattern
 fi
 
