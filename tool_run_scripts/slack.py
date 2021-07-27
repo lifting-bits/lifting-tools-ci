3240beccb3941067244e115059536fccfdcc67d7
Artem Dinaburg <artem@trailofbits.com>
2021-07-27 12:25:34 -0700

    Add a quick slack msg script

--- tool_run_scripts/slack.py
+++ tool_run_scripts/slack.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 import json
 import requests
 import sys
+import argparse
+import os
 
 class Slack:
     def __init__(self, hook, footer=None):
@@ -51,3 +54,38 @@ class Slack:
 
         if resp.status_code != 200:
             raise RuntimeError(f"Could not post slack messsage: [{resp.status_code}]: {resp.text}")
+
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--slack-hook",
+        default=os.environ.get("SLACK_HOOK", ""),
+        help="The slack hook to message. Defaults to SLACK_HOOK env var"
+    )
+
+    parser.add_argument(
+        "--header",
+        default="",
+        help="Header of the slack message, if any"
+    )
+
+    parser.add_argument("--msg", help="The message(s) to send", action="extend", nargs="+", type=str)
+
+    args = parser.parse_args()
+
+    msg_hook = args.slack_hook
+    if "" == msg_hook:
+        sys.stderr.write("Could not find a 'SLACK_HOOK' environment variable\n")
+        sys.exit(1)
+   
+    slack_msg = Slack(msg_hook)
+
+    if args.header != "":
+        slack_msg.add_header(args.header)
+
+    for message in args.msg:
+        slack_msg.add_block(message)
+
+    slack_msg.post()
+
